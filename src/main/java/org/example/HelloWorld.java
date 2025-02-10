183f5be56e5c6cff270c98d7ac3322f7f2b7db08
karth <karthavyakumar16111998@gmail.com>
2025-02-10 15:06:23 +0530

    HiiWelcome

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World From Neoteric");
+        System.out.println("Hello World From Neoteric 2025");
     }
 }
